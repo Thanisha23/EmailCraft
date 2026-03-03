02939f1ad112af38b72e2f9c3ecadcbf61de7ddb
Thanisha Belchada <thanishab77@gmail.com>
2026-03-03 15:41:59 +0530

    refactor: replace nodemailer with Brevo for email sending functionality

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,35 +1,27 @@
-import nodemailer from "nodemailer";
+import {BrevoClient} from "@getbrevo/brevo";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER!,
-    pass: process.env.EMAIL_PASS!,
-  },
-  pool: true,
-  maxConnections: 5,
-  rateDelta: 20000,
-  rateLimit: 5,
+const brevo = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY!,
 });
 
-export const sendEmail = async (to: string, subject: string, body: string) => {
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  body: string
+) => {
   try {
-    const mailOptions = {
-      from: `"EmailCraft App"${process.env.EMAIL_USER!}`,
-      to,
-      subject,
-      text: body,
-      headers: {
-        "X-Priority": "3",
-        "X-MSMail-Priority": "Normal",
-        Importance: "Normal",
-        "X-Mailer": "EmailCraft Mailer",
+    await brevo.transactionalEmails.sendTransacEmail({
+      sender: {
+        email: "noreply@thanisha.me",
+        name: "EmailCraft",
       },
-    };
+      to: [{ email: to }],
+      subject,
+      textContent: body,
+    });
 
-    await transporter.sendMail(mailOptions);
     console.log(`Email sent successfully to ${to}`);
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error("Brevo error:", error);
   }
 };
